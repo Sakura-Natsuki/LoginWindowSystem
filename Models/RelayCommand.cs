0a1c3f3e15711fc1ea2640a3f17508a83e3f48fa
Sakura-Natsuki <2065792501@qq.com>
2026-05-13 11:52:55 +0800

    1

--- Models/RelayCommand.cs
+++ Models/RelayCommand.cs
@@ -15,6 +15,7 @@ namespace LoginWindowSystem.Models
         //Predicate<object> 接受一个object参数、返回bool的方法
         private readonly Predicate<object> _canExecute;
 
+
         public RelayCommand(Action<object> execute,Predicate<object> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentException(nameof(execute));
